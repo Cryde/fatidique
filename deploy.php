df0390afd99c82797b3608c9e02cef7633818b57
Jérémy <t.jeremy.0@gmail.com>
2017-06-24 23:40:13 +0200

    chore: update + deployer stuff

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony3.php';
+require 'vendor/deployer/recipes/recipe/yarn.php';
+
+// Configuration
+
+set('repository', 'git@github.com:Cryde/fatidique.git');
+
+set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+inventory('hosts.yml');
+
+// Tasks
+desc('Build Brunch assets');
+task('assets:build', function () {
+    run("cd {{release_path}} && {{bin/yarn}} run build:prod");
+});
+after('yarn:install', 'assets:build');
+
+desc('Restart PHP-FPM service');
+task('php-fpm:restart', function () {
+    run('sudo systemctl restart php7.1-fpm.service');
+});
+after('deploy:symlink', 'php-fpm:restart');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'database:migrate');
+after('deploy:update_code', 'yarn:install');
