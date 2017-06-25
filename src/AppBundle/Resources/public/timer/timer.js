5eb922e56eae715b65e095813e0ab372d77f9f37
Jérémy <t.jeremy.0@gmail.com>
2017-06-25 12:31:18 +0200

    refactor: use moment and a plugin to calculate remaining time before the fatidique

--- src/AppBundle/Resources/public/timer/timer.js
+++ src/AppBundle/Resources/public/timer/timer.js
@@ -1,43 +1,40 @@
 import $ from "jquery";
+import moment from "moment";
+import precise from "moment-precise-range";
+const preciseDiff = precise(moment);
 
 export default () => {
   const $clock = $('#clock');
   if ($clock.length) {
 
-    const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+    const timestamp = parseInt($clock.data('timestamp'), 10) * 1000;
+    const deadline = new Date(timestamp);
     initializeClock($clock, deadline);
   }
 }
 
 function getTimeRemaining(endTime) {
-  const t = Date.parse(endTime) - Date.parse(new Date());
-  const seconds = Math.floor((t / 1000) % 60);
-  const minutes = Math.floor((t / 1000 / 60) % 60);
-  const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(t / (1000 * 60 * 60 * 24));
-
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
+  console.log(preciseDiff(endTime, new Date(), {returnObject: true}));
+  return preciseDiff(endTime, new Date(), {returnObject: true});
 }
 
 function initializeClock($clock, endTime) {
-  const daysSpan = $clock.find('.days');
-  const hoursSpan = $clock.find('.hours');
-  const minutesSpan = $clock.find('.minutes');
-  const secondsSpan = $clock.find('.seconds');
+  const $years = $clock.find('.years');
+  const $months = $clock.find('.months');
+  const $days = $clock.find('.days');
+  const $hours = $clock.find('.hours');
+  const $minutes = $clock.find('.minutes');
+  const $secondes = $clock.find('.seconds');
 
   function updateClock() {
     const t = getTimeRemaining(endTime);
 
-    daysSpan.text(t.days);
-    hoursSpan.text(('0' + t.hours).slice(-2));
-    minutesSpan.text(('0' + t.minutes).slice(-2));
-    secondsSpan.text(('0' + t.seconds).slice(-2));
+    $years.text(t.years);
+    $months.text(t.months);
+    $days.text(t.days);
+    $hours.text(('0' + t.hours).slice(-2));
+    $minutes.text(('0' + t.minutes).slice(-2));
+    $secondes.text(('0' + t.seconds).slice(-2));
 
     if (t.total <= 0) {
       clearInterval(timeInterval);
