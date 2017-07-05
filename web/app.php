c9e1909c88d0d9a8f162828f140128ba477db60e
Jérémy <t.jeremy.0@gmail.com>
2017-07-05 07:10:03 +0200

    style: remove commented code

--- web/app.php
+++ web/app.php
@@ -7,10 +7,6 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 
 $kernel = new AppKernel('prod', false);
 
-//$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
