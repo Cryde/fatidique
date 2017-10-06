cf7f3b20045e3fa1b63daf35d633fa7ec72e0ed9
Jérémy <t.jeremy.0@gmail.com>
2017-10-06 11:18:37 +0200

    chore: keep only 2 releases + delete previous node_modules dir

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@ require 'vendor/deployer/recipes/recipe/yarn.php';
 set('repository', 'git@github.com:Cryde/fatidique.git');
 
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
-set('keep_releases', 3);
+set('keep_releases', 2);
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
@@ -27,9 +27,15 @@ after('yarn:install', 'assets:build');
 
 desc('Restart PHP-FPM service');
 task('php-fpm:restart', function () {
-    run('sudo systemctl restart php7.1-fpm.service');
+    run('sudo service php7.1-fpm reload');
 });
 after('deploy:symlink', 'php-fpm:restart');
+
+desc('Remove node_modules folder');
+task('assets:clean', function() {
+    run('cd {{release_path}} && rm -rf node_modules');
+});
+after('deploy:symlink', 'assets:clean');
 
 // Migrate database before symlink new release.
 before('deploy:symlink', 'database:migrate');
