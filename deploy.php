358f8fa9acfe3b3c803fe5cebe0f2ecf776dde5d
Jérémy <jeremy.tonneau@stellar.io>
2021-08-30 08:24:13 +0200

    chore: missing file after upgrade

--- deploy.php
+++ deploy.php
@@ -2,56 +2,8 @@
 
 namespace Deployer;
 
-require 'recipe/symfony4.php';
-require 'vendor/deployer/recipes/recipe/npm.php';
-
-// Project repository
-set('repository', 'git@github.com:Cryde/fatidique.git');
-
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
-set('keep_releases', 2);
-set('ssh_multiplexing', true);
-add('shared_dirs', []);
-
-add('shared_files', [
-    '.env.local',
-]);
+require 'recipe/symfony.php';
+require 'contrib/npm.php';
 
 // Hosts
-inventory('hosts.yml');
-
-// Tasks
-task('npm:ci', function () {
-    run("cd {{release_path}} && {{bin/npm}} ci");
-});
-desc('Build assets');
-task(
-    'assets:build',
-    function () {
-        run('cd {{release_path}} && {{bin/npm}} run build');
-    }
-);
-after('npm:ci', 'assets:build');
-
-desc('Remove node_modules folder');
-task(
-    'assets:clean',
-    function () {
-        run('cd {{release_path}} && rm -rf node_modules');
-    }
-);
-after('deploy:symlink', 'assets:clean');
-
-desc('Restart PHP-FPM service');
-task(
-    'php-fpm:restart',
-    function () {
-        run('sudo service php7.4-fpm reload');
-    }
-);
-after('deploy:symlink', 'php-fpm:restart');
-
-// Migrate database before symlink new release.
-before('deploy:symlink', 'database:migrate');
-after('deploy:update_code', 'npm:ci');
+import('hosts.yml');
